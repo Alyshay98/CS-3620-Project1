d3a44fc2a262396c155aec7919d43ed29c339ee9
Alyshay98 <97034708+Alyshay98@users.noreply.github.com>
2024-01-11 19:01:53 -0700

    Finished
    Final commit and working

--- Project01.py
+++ Project01.py
@@ -127,7 +127,7 @@ class ChooseYourAdventure:
         if self.make_decision("From drinking the water you see a unicorn. Do you run away or become friends with it?",
                               "Become friends with the Unicorn",
                               "Run Away"):
-            print("\nYou approach the unicorn, and as a friendly gift, you are give a lock of unicorn hair.")
+            print("\nYou approach the unicorn, and as a friendly gift, you are given a lock of unicorn hair.")
             self.inventory.append("Unicorn Hair")
 
             time.sleep(1)
@@ -161,7 +161,7 @@ class ChooseYourAdventure:
         time.sleep(1)
 
         if self.make_decision("As you walk through the dark forest you come across some dwarves. "
-                              "They look might hostile though, Do you stay and fight or avoid them?",
+                              "They look mighty hostile though, Do you stay and fight or avoid them?",
                               "Stay and fight",
                               "Let's avoid them"):
             print("\nYou fight the dwarves off. Swords clash and you take a bit of damage. "
@@ -198,7 +198,7 @@ class ChooseYourAdventure:
                 self.player_health = self.player_health + 5
 
             time.sleep(1)
-            self.narrative_16()
+            self.narrative_9()
 
     def narrative_9(self):
         print("\nYour journey continues...")
@@ -212,10 +212,10 @@ class ChooseYourAdventure:
             time.sleep(1)
             self.narrative_10()
         else:
-            print("\nYYou decide to ask for some directions.")
+            print("\nYou decide to ask for some directions.")
 
             time.sleep(1)
-            self.narrative_18()
+            self.narrative_19()
 
     def narrative_10(self):
         print("\nYour journey continues...")
@@ -283,14 +283,18 @@ class ChooseYourAdventure:
         print("\nYour journey continues...")
         time.sleep(1)
 
-        if self.make_decision("As your journey continues, you come across a gorge"):
-            print("\nYou")
-            self.inventory.append("")
+        if self.make_decision("As your journey continues, you come across a gorge with a swinging rope"
+                              " bridge strung across it. Do you cross the rope bridge or find another way down?",
+                              "Cross the Rope Bridge",
+                              "Find another way down"):
+            print("\nYou decide to cross the rickety old bridge. It swings in the wind, "
+                  "testing the integrity of the rope that binds it together. "
+                  "You make it across just as it snaps and falls apart.")
 
             time.sleep(1)
             self.narrative_14()
         else:
-            print("\nYou decide to continue your journey outside the cave.")
+            print("\nYou decide to try find another way down the gorge")
 
             time.sleep(1)
             self.narrative_14()
@@ -299,14 +303,19 @@ class ChooseYourAdventure:
         print("\nYour journey continues...")
         time.sleep(1)
 
-        if self.make_decision(""):
-            print("\nYou")
-            self.inventory.append("")
+        if self.make_decision("After your journey across the gorge, you come down to the river. "
+                              "Do you try to find a bridge to cross or swim across it?",
+                              "Find a bridge",
+                              "Swim Across"):
+            print("\nYou decide to be safer than sorry and find a bridge to cross the river")
 
             time.sleep(1)
             self.narrative_15()
         else:
-            print("\nYou decide to continue your journey outside the cave.")
+            print("\nYou decide to swim across. As you swim across, you are attacked by a horde or crocodiles. "
+                  "You swim quicker for land to fight and fend off the crocodiles, gaining some teeth in the end.")
+            self.inventory.append("Crocodile Teeth")
+            self.player_health = self.player_health - 5
 
             time.sleep(1)
             self.narrative_15()
@@ -315,46 +324,59 @@ class ChooseYourAdventure:
         print("\nYour journey continues...")
         time.sleep(1)
 
-        if self.make_decision(""):
-            print("\nYou")
-            self.inventory.append("")
+        if self.make_decision("After your journey across the river, you come to a fork in the road. "
+                              "Do you head left to Dead Valley or right to the Grasslands?",
+                              "Head Right to the grasslands",
+                              "Head Left to Dead Valley"):
+            print("\nYou head off in the path of the grasslands.")
 
             time.sleep(1)
             self.narrative_16()
         else:
-            print("\nYou decide to continue your journey outside the cave.")
+            print("\nYou take the path off towards Dead Valley.")
 
             time.sleep(1)
-            self.narrative_16()
+            self.narrative_17()
 
     def narrative_16(self):
         print("\nYour journey continues...")
         time.sleep(1)
 
-        if self.make_decision(""):
-            print("\nYou")
-            self.inventory.append("")
+        if self.make_decision("After walking for a good amount of time, you start to feel tired. "
+                              "Do you stop to take a rest or continue on?",
+                              "Stop to take a rest",
+                              "Continue On"):
+            print("\nYou decide to take a rest")
+            if self.player_health == 100:
+                print("\nYour health is already refreshed")
+            else:
+                self.player_health = self.player_health + 5
 
             time.sleep(1)
-            self.narrative_17()
+            self.narrative_18()
         else:
-            print("\nYou decide to continue your journey outside the cave.")
+            print("\nYou decide to continue your journey.")
 
             time.sleep(1)
-            self.narrative_17()
+            self.narrative_18()
 
     def narrative_17(self):
         print("\nYour journey continues...")
         time.sleep(1)
 
-        if self.make_decision(""):
-            print("\nYou")
-            self.inventory.append("")
+        if self.make_decision("Headed off towards Dead Valley, you feel the crunch of the sands beneath your feet. "
+                              "Suddenly you hear something start to rustle. It's a Giant Sand Cicada! "
+                              "Do you Fight or will you make a dash for town?",
+                              "Fight the Cicada!",
+                              "Make a dash for town!"):
+            print("\nYou pull your sword out to defend your honor. You slash and hack away at the cicada. "
+                  "You are able to pull off the battle and walk away unscathed taking the wings along with you.")
+            self.inventory.append("Cicada Wings")
 
             time.sleep(1)
             self.narrative_18()
         else:
-            print("\nYou decide to continue your journey outside the cave.")
+            print("\nYou Make a mad dash for the nearest town.")
 
             time.sleep(1)
             self.narrative_18()
@@ -363,14 +385,16 @@ class ChooseYourAdventure:
         print("\nYour journey continues...")
         time.sleep(1)
 
-        if self.make_decision(""):
-            print("\nYou")
-            self.inventory.append("")
+        if self.make_decision("You finally make it to a town where you can enjoy the scenery. "
+                              "Do you stop to shop or find a quest to go on?",
+                              "Stop to shop around",
+                              "Go on a quest"):
+            print("\nYou decide to stay and shop around the town for a bit.")
 
             time.sleep(1)
             self.narrative_19()
         else:
-            print("\nYou decide to continue your journey outside the cave.")
+            print("\nYou decide its time to go on another quest!")
 
             time.sleep(1)
             self.narrative_19()
@@ -379,14 +403,16 @@ class ChooseYourAdventure:
         print("\nYour journey continues...")
         time.sleep(1)
 
-        if self.make_decision(""):
-            print("\nYou")
-            self.inventory.append("")
+        if self.make_decision("On your way out of town you decide where to go on a quest. "
+                              "Do you had for the Water lands or head for another unknown place?",
+                              "Head for the water lands",
+                              "Head for another unknown place"):
+            print("\nYou decide to head to the water lands for another adventure.")
 
             time.sleep(1)
             self.narrative_20()
         else:
-            print("\nYou decide to continue your journey outside the cave.")
+            print("\nYou decide to continue your journey in another unknown place")
 
             time.sleep(1)
             self.narrative_20()
@@ -412,6 +438,7 @@ class ChooseYourAdventure:
             print("Final Status: ")
             print(f"Player Name: {self.player_name}")
             print(f"Player Health: {self.player_health}")
+            print("Inventory: ", self.inventory)
 
 
 if __name__ == "__main__":
